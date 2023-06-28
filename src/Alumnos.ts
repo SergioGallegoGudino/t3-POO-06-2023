a9cfdb1ef0a071cdf22932c27dad6cb1628e63d9
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-06-28 18:42:50 +0200

    alumnos sgallego

--- src/Alumnos.ts
+++ src/Alumnos.ts
@@ -2,9 +2,21 @@ class Alumnos extends Personas{
 
     private calificacion:number;
 
-    constructor(newNombre:string, newEdad:number, newSexo:tipSexo, newAsistencia:boolean, newCalificacion:number){
-        super(newNombre, newEdad, newSexo, newAsistencia);
+    constructor(newNombre:string, newEdad:number, newSexo:tipSexo, newCalificacion:number, ){
+        super(newNombre, newEdad, newSexo);
         this.calificacion = newCalificacion;
+    }
+
+    comprobarDisponibilidad(): void{
+        this.disponible = 
+    }
+    
+    get disponibildiad(): boolean {
+        return this.disponible;
+    }
+    
+    get nota(): number {
+        return this.calificacion;
     }
 
 }
