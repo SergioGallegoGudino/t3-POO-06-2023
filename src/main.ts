0729d7661f9b563b55b3f1a89d804533676924c4
OrtziG <ortzi98@gmail.com>
2023-06-28 19:10:54 +0200

    Dejará este?

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 let estudiante1 = new Alumnos ("Paco", 12, "H", 7);
 let estudiante2 = new Alumnos ("Enrique", 13, "H", 4);
 let estudiante3 = new Alumnos ("Anna", 16, "M", 9);
-let estudiante4 = new Alumnos ("", , "", );
-let estudiante5 = new Alumnos ("", , "", );
-let estudiante6 = new Alumnos ("", , "", );
-let estudiante7 = new Alumnos ("", , "", );
-let estudiante8 = new Alumnos ("", , "", );
-let estudiante9 = new Alumnos ("", , "", );
-let estudiante10 = new Alumnos ("", , "", );
-let estudiante11 = new Alumnos ("", , "", );
+let estudiante4 = new Alumnos ("Luis", 10, "H", 2);
+let estudiante5 = new Alumnos ("Hermenegildo", 12, "H", 5);
+let estudiante6 = new Alumnos ("Romulana", 11, "M", 1);
+let estudiante7 = new Alumnos ("Purificación", 11, "M", 10);
+let estudiante8 = new Alumnos ("Julián", 12, "H", 9);
+let estudiante9 = new Alumnos ("Maribel", 15, "M", 4);
+let estudiante10 = new Alumnos ("Manolo", 14, "M", 8);
+let estudiante11 = new Alumnos ("Juana", 13, "M", 12);
+
+let prof1 = new Profesor ("", 46, "H","Física");
