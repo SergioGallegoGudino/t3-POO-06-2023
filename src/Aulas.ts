82dbd055f6be58bc8f2f9ab2bf1308b8e30509d3
AlbertEsponey4 <126797937+AlbertEsponey4@users.noreply.github.com>
2023-06-28 19:10:36 +0200

    Aulas tick

--- src/Aulas.ts
+++ src/Aulas.ts
@@ -4,20 +4,65 @@ class Aulas{
     private id: number;
     private maxAlumnos: number;
     private materia: materia;
-    private alumnos: Alumnos[];
-    private profesor: Profesores;
+    private alumnos: Alumnos[] | null;
+    private profesor: Profesores | null;
 
     constructor(id: number, maxAlumnos: number, materia: materia, alumnos: Alumnos[], profesor: Profesores){
         this.id = id;
         this.maxAlumnos = maxAlumnos;
         this.materia = materia;
-        if(alumnos.length >= (maxAlumnos/2)){
+
+        if(comprovarDisponibilidadClasse(alumnos, maxAlumnos)) 
             this.alumnos = alumnos;
-        }
-        if(profesor.getMateria == materia){
+        else 
+            this.alumnos = null;
+        
+        if(comprovarProfesor(profesor, materia))
             this.profesor = profesor;
+        else 
+            this.profesor = null;
+        
+    }
+
+    aprovados(){
+        let contM = 0;
+        let contH = 0;
+        if (this.alumnos == null || this.profesor == null){
+            return null
+        } else{
+            for (let i = 0; i < this.alumnos.length; i++){
+                if (this.alumnos[i].getSexo == 'H' && this.alumnos[i].getCalificacion >= 5){
+                    contH++;
+                }
+
+                if (this.alumnos[i].getSexo == 'M' && this.alumnos[i].getCalificacion >= 5){
+                    contM++;
+                }
+            }
+        console.log(`Alumnas aprovadas = ${contM}`);
+        console.log(`Alumnos aprovados = ${contH}`);
         }
     }
+}
+
+const comprovarDisponibilidadClasse = (alumnos: Alumnos[], maxAlumnos: number) => {
+    let cont = 0;
+    for (let i = 0; i < alumnos.length; i++){
+        if (!alumnos[i].getDisponible)
+            cont++;
+    }
+
+    if (cont >= maxAlumnos/2)
+        return true;
+    else
+        return false;
+}
+
+const comprovarProfesor = (profesor: Profesores, materia: materia) => {
+    if (profesor.getMateria === materia && profesor.getDisponible)
+        return true;
+    else
+        return false
 }
 
 
