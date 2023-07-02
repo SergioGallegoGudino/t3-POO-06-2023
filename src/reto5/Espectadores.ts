0d7029c0615900c1b9a1c9bd412f3af860d436bd
Sergio Gallego Gudiño <sergiogallegogudino@gmail.com>
2023-07-02 09:58:26 +0200

    ej5 sgallego

---
+++ src/reto5/Espectadores.ts
@@ -0,0 +1,12 @@
+class Espectadores{
+    private nombre:string;
+    private edad:number;
+    private dinero:number;
+
+    constructor(newNombre:string, newEdad:number, newDinero:number){
+        this.nombre = newNombre;
+        this.edad = newEdad;
+        this.dinero = newDinero;
+    }
+
+}
