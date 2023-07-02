dd9c62d7908f424d8d08b25f8ec32c5a8bd3c100
OrtziG <ortzi98@gmail.com>
2023-07-02 10:59:08 +0200

    reto 3 OGG

--- js/reto3/main.js
+++ js/reto3/main.js
@@ -1,1 +1,7 @@
 "use strict";
+// 
+const ecuacion = new Raices(1, 7, 3);
+// 
+ecuacion.calcular();
+ecuacion.obtenerRaices();
+ecuacion.obtenerRaiz();
---
+++ src/reto3/main.ts
@@ -0,0 +1,6 @@
+// 
+const ecuacion = new Raices(1, 7, 3);
+// 
+ecuacion.calcular();
+ecuacion.obtenerRaices();
+ecuacion.obtenerRaiz();
